a6f98aafbe3a8f35cd886255660119924ad64b11
PRAKASH_GADHAM <prakash.gadham.dev@gmail.com>
2026-06-30 07:19:08 +0530

    Time: 3 ms (46.09%), Space: 42 MB (69.4%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1){
+        if(n <= 1 || n == 5){
             return n ;
+        }
+        if(n <= 4 && n > 1){
+            return n-1 ;
         }
         else{
             return fib(n-1)+fib(n-2) ;
