a37933d5c98fccbf3d4232d65cec6aa8ed5a0fbb
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-08-01 10:18:55 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,23 @@
+class Solution {
+    static{
+        for(int i = 0 ; i < 200 ; i++){
+            predictTheWinner(new int[]{1,1}) ;
+        }
+    }
+    public static boolean predictTheWinner(int[] nums){
+        int n = nums.length ;
+        int[][] dp = new int[n][n] ;
+
+        for(int i = 0 ; i < n ; i++){
+            dp[i][i] = nums[i] ;
+        }
+
+        for(int len = 2 ; len <= n ; len++){
+            for(int i = 0 ; i <= n - len ; i++){
+                int j = i + len - 1 ;
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]) ;
+            }
+        }
+        return dp[0][n - 1] >= 0 ;
+    }
+}   
