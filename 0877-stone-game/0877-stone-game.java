e9b0f7df4999e268c84def19d130a42f20349a0b
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-08-02 12:10:22 +0530

    Time: 0 ms (100%), Space: 42.9 MB (88.59%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,51 +1,5 @@
 class Solution {
-    record Selection(int max , int i ,int j){}
-
     public boolean stoneGame(int[] piles) {
-        int alice = 0 ;
-        int bob = 0 ;
-        int i = 0 ;
-        int j = piles.length - 1 ;
-        while(i < j){
-            Selection res = Alice(piles , i , j) ;
-            alice += res.max() ;
-            i = res.i() ;
-            j = res.j() ;
-
-            Selection res1 = Bob(piles , i , j) ;
-            bob += res1.max() ;
-            i = res1.i() ;
-            j = res1.j() ;
-        }
-        if(alice > bob){
-            return true ;
-        }
-        return false ;
-    }
-
-    private Selection Alice(int[] piles,int i ,int j){
-        int max = 0 ;
-        if(piles[i] >= piles[j]){
-            max = piles[i] ;
-            i++ ;
-        }
-        else{
-            max = piles[j] ;
-            j-- ;
-        }
-        return new Selection(max , i , j) ;
-    }
-
-    private Selection Bob(int[] piles,int i ,int j){
-        int max = 0 ;
-        if(piles[i] <= piles[j]){
-            max = piles[i] ;
-            i++ ;
-        }
-        else{
-            max = piles[j] ;
-            j-- ;
-        }
-        return new Selection(max , i , j) ;
+        return true ;
     }
 }
