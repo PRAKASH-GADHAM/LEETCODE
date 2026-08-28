213d999840fef06b26854797c726a9eff2c3b2c0
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-08-28 18:39:09 +0530

    Time: 2 ms (96.99%), Space: 52.9 MB (38.55%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.java
@@ -1,22 +1,11 @@
 class Solution {
     public int numTimesAllBlue(int[] flips) {
-        boolean[] prefix = new boolean[flips.length] ;
-        int n = flips.length ;
+        int fliped = 0 ;
         int count = 0 ;
 
-        for(int i = 0 ; i < n ; i++){
-            int flip = flips[i] ;
-            prefix[flip - 1] = true ;  
-            boolean found = true ;
-
-            for(int j = 0 ; j <= i ; j++){
-                if(!prefix[j]){
-                    found = false ;
-                    break ;
-                }
-            }
-
-            if(found) count++ ;
+        for(int i = 0 ; i < flips.length ; i++){
+            fliped = Math.max(fliped , flips[i]) ;
+            if(fliped == i+1) count++ ;
         }
         return count ;
     }
