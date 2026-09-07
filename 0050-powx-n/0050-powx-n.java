0f8cac1b70a53aa2aaa75ed4aab0b55719eb9309
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-09-07 19:37:17 +0530

    Time: 0 ms (100%), Space: 48 MB (56.3%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,33 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res = 1.00d ;
-        if(n == 0 || x == 1d){
-            res = 1.00d ;
-        }
-        else if(x == -1d){
-            if(n % 2 == 0){
-                res = 1.00d ;
-            }
-            else{
-                res = -1.00d ;
-            }
-        }
-        else if(n <= Integer.MIN_VALUE || n >= Integer.MAX_VALUE){
-            res = 0.00d ;
-        }
-        else if(n < 0){
-            while(n != 0){
-                res /= x ;
-                n++ ;
-            }
-        }
-        else{
-            while(n != 0){
-                res *= x ;
-                n-- ;
-            }
+        if (n == 0) return 1.0 ;
+        long N = n ;
+
+        if(N < 0){
+            x = 1 / x ;
+            N = -N ;
         }
 
-        return res ;    
+        double result = 1.0 ;
+        double currentProduct = x ;
+
+        while(N > 0){
+            if((N % 2) == 1){
+                result *= currentProduct ;
+            }
+            currentProduct *= currentProduct ;
+            N /= 2 ;
+        }
+        return result ;
     }
 }
