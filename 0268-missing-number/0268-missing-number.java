869d64aa7278958394b0e5c9fbe8277e91b31686
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-09-11 18:39:08 +0530

    Time: 0 ms (100%), Space: 47.4 MB (32.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums) ;
-        int ans = 0 ;
-        int[] hash = new int[nums.length + 1] ;
-        for(int num : nums){
-            hash[num] = num ; 
-        }
-        for(int i = 0 ; i < nums.length + 1 ; i++){
-            ans = ans ^ i ^ hash[i] ;
-        }
-        return ans ;
+        int xor = nums.length ;
+        for(int i = 0 ; i < nums.length ; i++){
+            xor ^= i ^ nums[i] ;
+        }   
+        return xor ;
     }
 }
