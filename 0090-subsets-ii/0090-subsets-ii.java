b5b29cb8c6f85892497632eefe4684245e65b323
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-09-12 09:39:26 +0530

    Time: 2 ms (99.74%), Space: 44.9 MB (88.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums) ; 
-        int total_subsets = 1 << nums.length ;
-        List<List<Integer>> list = new ArrayList<>() ;
-        list.add(new ArrayList<>()) ;
+        List<List<Integer>> result = new ArrayList<>() ;
+        List<Integer> list = new ArrayList<>() ;
+        Arrays.sort(nums) ;
+        calculate(nums,0,list,result) ;
+        return result ;
+    }
 
-        for (int i = 1; i < total_subsets; i++) {
-            List<Integer> l = new ArrayList<>() ;
-            boolean isValid = true ;
+    void calculate(int[] nums , int index , List<Integer> list , List<List<Integer>> result) {
+        result.add(new ArrayList<>(list)) ;
 
-            for (int j = 0; j < nums.length; j++) {
-                if ((i & (1 << j)) != 0) {
-                    if (j > 0 && nums[j] == nums[j - 1] && (i & (1 << (j - 1))) == 0) {
-                        isValid = false ;
-                        break ;
-                    }
-                    l.add(nums[j]) ;
-                }
-            }
-
-            if (isValid) {
-                list.add(l) ;
-            }
+        for(int i = index ; i < nums.length ; i++){
+            if(i > index && nums[i] == nums[i - 1]) continue ;
+            list.add(nums[i]) ;
+            calculate(nums , i+1 , list , result) ;
+            list.remove(list.size() - 1);
         }
-        return list ;
     }
 }
