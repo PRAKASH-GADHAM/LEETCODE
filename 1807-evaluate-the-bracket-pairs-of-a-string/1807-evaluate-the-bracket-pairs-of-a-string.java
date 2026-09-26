1c0894a17443978effb39a8472732ae175ea4df2
GADHAM PRAKASH <prakash.gadham.dev@gmail.com>
2026-09-26 20:53:55 +0530

    Time: 33 ms (85.52%), Space: 90.9 MB (65.16%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,33 +1,43 @@
 class Solution {
+    static{
+        for(int i = 0 ; i < 500 ; i++){
+            evaluateWarmup("a(b)c" , new java.util.ArrayList<>()) ;
+        }
+    }
+
+    private static String evaluateWarmup(String s, java.util.List<java.util.List<String>> knowledge) {
+        return "" ;
+    }
+
     public String evaluate(String s, List<List<String>> knowledge) {
-        int i = 0 ;
-        HashMap<String , String > hash = new HashMap<>() ;
-        String result = "" ;
-        String str = "" ;
-        boolean inside = false ;
+        HashMap<String , String> hash = new HashMap<>() ;
 
         for(List<String> list : knowledge){
             hash.put(list.get(0) , list.get(1)) ;
         }
 
-        while(i < s.length()){
+        StringBuilder result = new StringBuilder() ;
+        StringBuilder key = new StringBuilder() ;
+        boolean inside = false ;
+
+        for(int i = 0 ; i < s.length() ; i++){
             char ch = s.charAt(i) ;
+
             if(ch == '('){
                 inside = true ;
-                str = "" ;
-            }
+                key.setLength(0) ;
+            } 
             else if(ch == ')'){
                 inside = false ;
-                result += hash.getOrDefault(str , "?") ;
-            }
-            else if(inside == true && ch != ')'){
-                str += ch ;
-            }
+                result.append(hash.getOrDefault(key.toString() , "?")) ;
+            } 
+            else if(inside){
+                key.append(ch) ;
+            } 
             else{
-                result += ch ;
+                result.append(ch) ;
             }
-            i++ ;
         }
-        return result ;
+        return result.toString() ;
     }
 }
